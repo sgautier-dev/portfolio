1f18a3cd3af1a3fd9e938a81f069b2f8bac1c1a9
sgautier-dev <sgautier.dev@gmail.com>
2022-11-04 17:57:19 +0400

    added header and  hero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
 /*init project with <npx tailwindcss init>
   build and watch with <npx tailwindcss -i ./src/input.css -o ./build/css/style.css --watch>
+  init package.json with <npm init -y>
+  install prettier for tailwind with <npm i -D prettier-plugin-tailwindcss>
 */
 /** @type {import('tailwindcss').Config} */
 module.exports = {
