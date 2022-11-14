cd0c01cdd7b9c342010c284a25f5dbd3d73b5ade
sgautier-dev <sgautier.dev@gmail.com>
2022-11-14 10:55:13 +0400

    header opacity added on scroll

--- build/js/main.js
+++ build/js/main.js
@@ -1,6 +1,13 @@
 const initApp = () => {
     const hamburgerBtn = document.getElementById('hamburger-button')
     const mobileMenu = document.getElementById('mobile-menu')
+    const header = document.getElementsByTagName('header')[0]
+
+    const yearSpan = document.getElementById('year')
+    const today = new Date()
+    const year = today.getFullYear()
+
+    const scrollTrigger = 60;
 
     const toggleMenu = () => {
         mobileMenu.classList.toggle('hidden')
@@ -10,15 +17,13 @@ const initApp = () => {
 
     hamburgerBtn.addEventListener('click', toggleMenu)
     mobileMenu.addEventListener('click', toggleMenu)
-}
-
-const displayCurrentYear = () => {
-    const yearSpan = document.getElementById('year')
-    const today = new Date()
-    const year = today.getFullYear()
 
     yearSpan.innerText = year
+
+    document.addEventListener('scroll', (e) => {
+        window.scrollY >= scrollTrigger ? header.classList.add('opacity-90') : header.classList.remove('opacity-90')
+    })
+
 }
 
 document.addEventListener('DOMContentLoaded', initApp)
-document.addEventListener('DOMContentLoaded', displayCurrentYear)
