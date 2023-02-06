a4ffac7a1fb150064216ea46f738e204ecbae790
sgautier-dev <sgautier.dev@gmail.com>
2023-02-06 14:37:11 +0400

    env.js deleted

--- env.js
+++
@@ -1,8 +0,0 @@
-const env = {
-    NODE_ENV: "development",
-    EMAIL_PUBLIC_KEY: "qxxzJq5vCOVNRww2i",
-    EMAIL_TEMPLATE_ID: "template_dx7svpf",
-    EMAIL_SERVICE_ID: "service_86j0nfr",
-  };
-  
-  window.env = env;
